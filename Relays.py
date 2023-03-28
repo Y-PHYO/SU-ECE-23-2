5d7f6123969a1596f4ec32d246d9d89037747177
ECE-23-2 <124209424+ECE-23-2@users.noreply.github.com>
2023-03-28 10:14:00 -0700

    Add files via upload

---
+++ Relays.py
@@ -0,0 +1,15 @@
+light1 = 21
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(light1, GPIO.OUT)
+while True:
+     GPIO.output(light1, True)
+     time.sleep(2)
+     GPIO.output(light1, False)
+     time.sleep(2)
+    
+    
+GPIO.setwarnings(False)  
+GPIO.cleanup()
